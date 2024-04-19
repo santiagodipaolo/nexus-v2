5d06206e7bbb5261e452b6e41798933e4334b9a6
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2024-04-19 17:43:26 -0300

    fixed text color

--- src/components/FAQ/index.js
+++ src/components/FAQ/index.js
@@ -8,7 +8,7 @@ const Faq = () => {
   return (
     <div className="flex flex-col justify-center xs:p-10 md:p-0 items-center bg-[url('/faq-bg.png')] xs:h-[700px] md:h-[505px] w-full">
         <h1 className="font-semibold text-black text-3xl text-center mt-10 mb-10">Frequently asked <i>questions</i></h1>
-        <div className="flex flex-row mb-16">
+        <div className="flex flex-row mb-16 text-white">
             <div className="join join-vertical w-full md:w-[600px] xl:w-[800px] bg-black xl:h-[310px] lg:mr-10">
                 <div className="collapse collapse-arrow join-item border border-base-300">
                     <input type="radio" name="my-accordion-4" defaultChecked /> 
