b877b5d0ad140e6f05d9a248322e929b0449e141
Bautista1405 <80073511+bautista1405@users.noreply.github.com>
2024-05-10 18:18:11 -0300

    updated benefits table

--- src/components/Benefits/index.js
+++ src/components/Benefits/index.js
@@ -5,53 +5,41 @@ const BenefitsTable = () => {
   return (
     <div id="about" className="flex flex-col justify-center xs:p-10 md:p-0 items-center mt-20 bg-[url('/background-1.png')] w-full">
         <Image alt="arrow down" style={{width:"30px", height:"auto"}} src={ArrowDown} className="mt-5" />
-        <h1 className="font-semibold text-white text-3xl text-center mt-10 mb-10">We&apos;ve got the <br/> best of <i>both worlds</i></h1>
+        <h1 className="font-semibold text-white text-3xl text-center mt-10 mb-10">Why hire in <i>LatAm?</i></h1>
         <div className="overflow-x-auto xs:max-w-xs md:max-w-xl mb-[-70px] shadow-xl">
             <table className="table border border-black text-center">
                 {/* head */}
                 <thead className="bg-black text-white text-lg">
                 <tr>
-                    <th className="border-r border-gray-500"><i>great benefits</i></th>
-                    <th className="border-r border-gray-500"><i>for your startup</i></th>
-                    <th><i>for our talent</i></th>
+                    <th className="border-r border-gray-500"><i>Benefits</i></th>
+                    <th className=""><i>For your business</i></th>
                 </tr>
                 </thead>
                 <tbody className="bg-[#F8F5F2] text-black">
                 {/* row 1 */}
                 <tr>
-                    <th className="border-r border-gray-500"><i>Payment</i></th>
-                    <td className="border-r border-gray-500">Cy Ganderton</td>
-                    <td>Quality Control Specialist</td>
+                    <th className="border-r border-gray-500"><i>Lower cost</i></th>
+                    <td className="border-r border-gray-500">Depending on the role, salaries in Latin America can save between 40% and 70% for the company. </td>
                 </tr>
                 {/* row 2 */}
                 <tr>
-                    <th className="border-r border-gray-500"><i>Time Zone</i></th>
-                    <td className="border-r border-gray-500">Hart Hagerty</td>
-                    <td>Desktop Support Technician</td>
+                    <th className="border-r border-gray-500"><i>Cultural Fit</i></th>
+                    <td className="border-r border-gray-500">Latin American professionals excel in US companies by adapting to dynamic cultures and mirroring dedicated work ethic, spirit and agilityy</td>
                 </tr>
                 {/* row 3 */}
                 <tr>
-                    <th className="border-r border-gray-500"><i>Opportunities</i></th>
-                    <td className="border-r border-gray-500">Brice Swyre</td>
-                    <td>Tax Accountant</td>
+                    <th className="border-r border-gray-500"><i>US Times Zones</i></th>
+                    <td className="border-r border-gray-500">LatAm and US time zones are typically aligned, making coordination and communication between the regions convenient and efficient. </td>
                 </tr>
                 {/* row 4 */}
                 <tr>
-                    <th className="border-r border-gray-500"><i>Convinience</i></th>
-                    <td className="border-r border-gray-500">Brice Swyre</td>
-                    <td>Tax Accountant</td>
+                    <th className="border-r border-gray-500"><i>Strong English</i></th>
+                    <td className="border-r border-gray-500">Many people have a high level of English proficiency, allowing them to communicate well in the global professional market.</td>
                 </tr>
                 {/* row 5 */}
                 <tr>
-                    <th className="border-r border-gray-500"><i>Trouble Shooting</i></th>
-                    <td className="border-r border-gray-500">Brice Swyre</td>
-                    <td>Tax Accountant</td>
-                </tr>
-                {/* row 6 */}
-                <tr>
-                    <th className="border-r border-gray-500"><i>Management</i></th>
-                    <td className="border-r border-gray-500">Brice Swyre</td>
-                    <td>Tax Accountant</td>
+                    <th className="border-r border-gray-500"><i>High Motivation</i></th>
+                    <td className="border-r border-gray-500">Low salaries in LatAm make the possibility of earning a US dollar salary a strong motivator for workers in the region.</td>
                 </tr>
                 </tbody>
             </table>
