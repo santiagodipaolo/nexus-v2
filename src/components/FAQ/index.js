8c21f6bcad3ca056ad4f013c03e7730a6e6f1191
Dipa <84927649+santiagodipaolo@users.noreply.github.com>
2024-05-24 16:26:51 -0300

    Update index.js

--- src/components/FAQ/index.js
+++ src/components/FAQ/index.js
@@ -22,19 +22,19 @@ const Faq = () => {
                 <div className="collapse collapse-arrow join-item border border-base-300">
                     <input type="radio" name="my-accordion-4" /> 
                     <div className="collapse-title text-xl font-medium">
-                        How is my employee&apos;s salary handled? 
+                        How is my contractor&apos;s salary handled? 
                     </div>
                     <div className="collapse-content"> 
-                        <p>You can trust us to take care of your payroll needs with our Staffing model, so that you can focus on your core business operations. We have an efficient and reliable payroll system in place to ensure that cross-border payments to  your employees are paid on time and in compliance with all relevant regulations.</p>
+                        <p>You can trust us to take care of your payroll needs with our Staffing model, so that you can focus on your core business operations. We have an efficient and reliable payroll system in place to ensure that cross-border payments to  your contractors are paid on time and in compliance with all relevant regulations.</p>
                     </div>
                 </div>
                 <div className="collapse collapse-arrow join-item border border-base-300">
                     <input type="radio" name="my-accordion-4" /> 
                     <div className="collapse-title text-xl font-medium">
-                        What procedures do I need to follow to hire an international employee?
+                        What procedures do I need to follow to hire an international contractor?
                     </div>
                     <div className="collapse-content"> 
-                        <p>Hiring an international employee can be a complex process, involving various legal and bureaucratic requirements. However, you don&apos;t need to worry about any of that! We can handle all of the paperwork and bureaucracy on your behalf. There are no extra costs or hidden fees involved; everything is included in the flat fee that we quote you.</p>
+                        <p>Hiring an international contractor can be a complex process, involving various legal and bureaucratic requirements. However, you don&apos;t need to worry about any of that! We can handle all of the paperwork and bureaucracy on your behalf. There are no extra costs or hidden fees involved; everything is included in the flat fee that we quote you.</p>
                     </div>
                 </div>
                 <div className="collapse collapse-arrow join-item border border-base-300">
@@ -49,7 +49,7 @@ const Faq = () => {
                 <div className="collapse collapse-arrow join-item border border-base-300">
                     <input type="radio" name="my-accordion-4" /> 
                     <div className="collapse-title text-xl font-medium">
-                        Can I provide a computer or other work related items to my employee? 
+                        Can I provide a computer or other work related items to my contractor? 
                     </div>
                     <div className="collapse-content"> 
                         <p>Yes, certainly. This can be discussed during the hiring process and we can assist you.</p>
